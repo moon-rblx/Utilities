f1e28367fa3a0405ec6066fb0e7a4639183b72d9
moon-rblx <moonrblx@icloud.com>
2025-09-19 17:39:36 -0500

    Create Functions.lua

---
+++ Functions.lua
@@ -0,0 +1,5 @@
+return {
+    Time = loadstring(game:HttpGet('https://raw.githubusercontent.com/moon-rblx/Utilities/refs/heads/main/Math/Time.lua'))(),
+    DeepCopy = loadstring(game:HttpGet('https://raw.githubusercontent.com/moon-rblx/Utilities/refs/heads/main/Table/DeepCopy.lua'))(),
+    DeepSearch = loadstring(game:HttpGet('https://raw.githubusercontent.com/moon-rblx/Utilities/refs/heads/main/Table/DeepSearch.lua'))()
+}
